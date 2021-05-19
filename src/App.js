d470c7aff34708cc85f243bc30f7ef730bd88126
MarGolNZ <54025171+MarGolNZ@users.noreply.github.com>
2021-05-19 18:17:36 +1000

    Add header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="container">
-       
+       <h1>Dictionary</h1>
+       <a href="#" className="btn btn-primary">Search</a>
        </div>
       </header>
     </div>
